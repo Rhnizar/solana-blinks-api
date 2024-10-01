210b729124b76271d5016673ca1efd5cbd2ae74e
Rhnizar <ridarhnizar1337@gmail.com>
2024-10-01 13:36:49 +0100

    Set up structured project layout with Django REST API for Solana Actions, integrated with Docker

--- backend/src/solana_api_server/settings.py
+++ backend/src/solana_api_server/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'solana_actions', # new
+    'corsheaders', # new
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware', # new
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8080",  # Allow your frontend origin
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'solana_api_server.urls'
 
